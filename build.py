7cd3dea115bed830f092edb79df8ace93a16bac7
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-07-01 23:26:59 -0300

    fix march

--- build.py
+++ build.py
@@ -23,13 +23,13 @@ if __name__ == "__main__":
                 # options["%s:with_benchmark" % name] = "True"
                 options["%s:with_tests" % name] = "True"
                 # options["%s:with_openssl_tests" % name] = "True"
-                marchs = ["x86_64"]
+                marchs = ["x86-64"]
             else:
                 if full_build:
-                    # marchs = ["x86_64", ''.join(cpuid.cpu_microarchitecture()), "haswell", "skylake", "skylake-avx512"]
+                    # marchs = ["x86-64", ''.join(cpuid.cpu_microarchitecture()), "haswell", "skylake", "skylake-avx512"]
                     marchs = [''.join(cpuid.cpu_microarchitecture()), 'znver1', 'silvermont', 'westmere', 'goldmont', 'btver1', 'icelake-client', 'btver2', 'skylake', 'nano', 'haswell', 'nano-1000', 'broadwell', 'bdver1', 'bdver3', 'bdver2', 'bdver4', 'core2', 'k8', 'amdfam10', 'icelake-server', 'bonnell', 'cannonlake', 'k8-sse3', 'goldmont-plus', 'nano-x4', 'nehalem', 'ivybridge', 'eden-x4', 'x86-64', 'nano-3000', 'knl', 'knm', 'penryn', 'eden-x2', 'sandybridge', 'nano-2000', 'tremont', 'skylake-avx512', 'nano-x2']
                 else:
-                    marchs = ["x86_64"]
+                    marchs = ["x86-64"]
 
             handle_microarchs("%s:microarchitecture" % name, marchs, filtered_builds, settings, options, env_vars, build_requires)
             # filtered_builds.append([settings, options, env_vars, build_requires])
